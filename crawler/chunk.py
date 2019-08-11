25e0bd77175484565deb60df3fbce01b1c6baddd
Michael <mikequackenbush1204@gmail.com>
2019-08-11 02:58:38 -0400

    Update chunk.py

--- crawler/chunk.py
+++ crawler/chunk.py
@@ -6,12 +6,8 @@
                         
  :"" in document['doc_header']:      f.write(item)     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header']     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header'"" in document['doc_header']:      f.write(item)     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header']     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header']:
 :"" in document['doc_header']:
-:"""" in self.documents:item in#!/usr/bin/env python3def __init__(self, import loggingself.header = []self.header = []create_chunk(self):else:document in self.documents:item in document['doc_header']:
-                  """ in """" in self.documents:item in#!/usr/bin/env python3def __init__(self, import loggingself.header = []self.header = []create_chunk(self):else:document in self.documents:item in document['doc_header']:
-                        f.write(item)     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header']:
-:"" in document['doc_header']:      f.write(item)     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header']:
-:""
-    desc: Chunk class, defines how to create and write data to a chunk
+:"""" :"" in document['doc_header']:      f.write(item)     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header']     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header'"" in document['doc_header']:      f.write(item)     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header']     # for now if crawler tries to add too many documents to a chunk an exception is raised document['doc_header']:
+
           Chunks are binary files. The last 20 bytes of a chunk is the header that can be used to seek to specific
           documents in the chunk.
 """
